71fd4bd1b5186421ea51afb515c0409f4cf82cfa
Bruna Guarizo <brunaguarizo@Mac.lan>
2024-10-20 11:40:37 -0700

    colors

---
+++ test.js
@@ -0,0 +1,28 @@
+console.log("Welcome to the amazing Metal Person");
+console.log("Another title");
+console.log("\x1b[32m", "Hi there")
+console.log("\x1b[0m", "Hi there")
+console.log('\x1b[46m', 'hello')
+console.log("\x1b[35m", "hi","\x1b[34m", "there")
+console.log("\x1b[35m", "hi there")
+console.log("\x1b[0m", "Hi there")
+console.log("\x1b[35m", "hi there")
+console.log("\x1b[0m", "hi there")
+console.log("\x1b[35m", "hi", "\x1b[34m", "there")
+
+console.log("\x1b[0m", "hi there")
+console.log("\x1b[45m", "hi there")
+console.log("\x1b[0m", "hi there")
+console.log("\x1b[35m", "hi", "\x1b[34m", "there")
+console.log("\x1b[0m", "hi there")
+console.log("\x1b[35m", "hi", "\x1b[34m", "there", "\x1b[32m", 'rainbow', "\x1b[33m", "colors")
+console.log("\x1b[0m", "hi there")
+console.log("\x1b[35m", "hi", "\x1b[34m", "there", "\x1b[32m", 'rainbow', "\x1b[33m", "colors", "\x1b[31m", "red one")
+console.log("\x1b[35m", "hi", "\x1b[34m", "there", "\x1b[32m", 'rainbow', "\x1b[33m", "colors")
+console.log("\x1b[34m","\x1b[47m","hi");
+console.log("\x1b[35m", "hi", "\x1b[34m", "there", "\x1b[32m", 'rainbow', "\x1b[33m", "colors")
+console.log("\x1b[0m","hi");
+console.log("\x1b[0m", "hi", "\x1b[0m", "there", "\x1b[0m", 'rainbow', "\x1b[0m", "colors")
+
+
+
